428ae58ac57cf34759a20debf0e1b2fdbde6def7
NooN <122496356+Enemy6@users.noreply.github.com>
2023-01-20 07:41:48 +1000

    Update script.js

--- script.js
+++ script.js
@@ -10,15 +10,3 @@ var words = xhr.responseText.split("\n");
 var results = words.filter(function(word) {
   return word.indexOf(searchQuery) !== -1;
 });
-var ul = document.createElement("ul");
-for (var i = 0; i < results.length; i++) {
-  var li = document.createElement("li");
-  li.innerHTML = results[i];
-  ul.appendChild(li);
-}
-document.body.appendChild(ul);
-document.querySelector("form").addEventListener("submit", function(event) {
-  event.preventDefault();
-  var searchQuery = document.querySelector("input[name='search']").value;
-  search(searchQuery);
-});
